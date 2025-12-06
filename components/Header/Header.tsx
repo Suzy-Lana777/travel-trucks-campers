5f19588d3ec8cebdbd289fae242a314beb49329e
Suzy-Lana777 <reutasvetlana382@gmail.com>
2025-12-06 08:28:41 +0200

    header+hero

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,21 +1,37 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import Container from "@/components/Container/Container";
 import css from "./Header.module.css";
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className={css.header}>
       <Container>
         <div className={css.inner}>
-          <div className={css.logo}>TravelTrucks</div>
+          <div className={css.logo}>
+            <svg width="136" height="15">
+              <use href="/icons/sprite.svg#logo" />
+            </svg>
+          </div>
 
           <nav className={css.nav}>
-            <Link href="/" className={css.link}>
+            <Link
+              href="/"
+              className={`${css.link} ${pathname === "/" ? css.active : ""}`}
+            >
               Home
             </Link>
-            <Link href="/catalog" className={css.link}>
+
+            <Link
+              href="/catalog"
+              className={`${css.link} ${
+                pathname.startsWith("/catalog") ? css.active : ""
+              }`}
+            >
               Catalog
             </Link>
           </nav>
